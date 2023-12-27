13653af472001dc4977807bfe194aa701244eefc
khalifardy <khalifardy.miqdarsah@gmail.com>
2023-12-27 14:45:04 +0700

    signup

--- apps/auth2/urls.py
+++ apps/auth2/urls.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from .views import Login
+from .views import Login, SignupStaff
 
 urlpatterns = [
-    re_path(r'^login/$', Login.as_view(), name='Login')
+    re_path(r'^login/$', Login.as_view(), name='Login'),
+    re_path(r'^signup/staff/$', SignupStaff.as_view(), name='Login')
 ]
--- apps/auth2/views.py
+++ apps/auth2/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.core.cache import cache
+from django.contrib.auth.hashers import make_password
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -10,6 +11,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework_jwt.settings import api_settings
 
 from apps.staff.models import StaffProfile, Role
+import datetime
 # Create your views here.
 
 
@@ -42,3 +44,42 @@ class Login(APIView):
             return Response(msg, status=status.HTTP_200_OK)
         else:
             return Response({"msg": "username atau password salah"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SignupStaff(APIView):
+
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        first_name = request.data.get('first_name')
+        last_name = request.data.get('last_name')
+        nik = request.data.get('nik')
+        email = request.data.get('email', None)
+        kontak = request.data.get('kontak', None)
+        contract_date = request.data.get('contract_date')
+        bod = request.data.get('bod')
+        role = request.data.get('role')
+        user_name = request.data.get('username')
+        password = request.data.get('password')
+
+        role_dict = {
+            "MANAGEMENT": 1,
+            "RESEPSIONIS": 2,
+            "ROOM_SERVICE": 3,
+            "CHEF": 4,
+            "SECURITY": 5,
+        }
+
+        try:
+            kontrak_date = datetime.datetime.strptime(
+                contract_date, "%Y-%m-%d")
+            bod = datetime.datetime.strptime(bod, "%Y-%m-%d")
+            hashed_password = make_password(password)
+            User.objects.create(username=user_name, password=hashed_password)
+            obj = User.objects.get(username=user_name)
+            Role.objects.create(user=obj, tipe=role_dict[role])
+            StaffProfile.objects.create(
+                full_name=first_name+" "+last_name, nik=nik, email=email, kontak=kontak, bod=bod, contract_date=kontrak_date, user=obj)
+            return Response({"message": "OK"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
